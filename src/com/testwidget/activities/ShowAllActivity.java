3a3b82f73f3e6659d7b5f616ca759f8983596a78
BigBlackBug <bigblackbugg@gmail.com>
2013-08-20 16:06:45 +0400

    added a mockup for implementing the editing of the card name. i've wasted soooo much time on this =__=

--- src/com/testwidget/activities/ShowAllActivity.java
+++ src/com/testwidget/activities/ShowAllActivity.java
@@ -1,16 +1,28 @@
 package com.testwidget.activities;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.nfc.NfcAdapter;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ImageView.ScaleType;
 import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
 import android.widget.TextView;
+import android.widget.TextView.OnEditorActionListener;
 
 import com.testwidget.App;
 import com.testwidget.CardDescriptor;
@@ -27,10 +39,20 @@ public class ShowAllActivity extends Activity {
 	
 	private String cardNumber;
 	private DataProvider dp = App.getDataProvider();
+	private Resources resources;
+	private int actionCode;
+	
+	private ImageView editImageView;
+	private EditText nameValueEditText;
+	private View mainScrollView;
+	private TextView valueTextView ;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		this.resources = getResources();
+		this.actionCode = resources.getInteger(R.integer.edit_text_action_code);
+		
 		setContentView(R.layout.show_all_layout);
 		
 		Intent intent = getIntent();
@@ -39,6 +61,65 @@ public class ShowAllActivity extends Activity {
 		
 		CardDescriptor cd = dp.getByNumber(cardNumber);
 		prepareTextViews(cd);
+		valueTextView = (TextView) findViewById(R.id.__name_value_tf);
+		editImageView = (ImageView) findViewById(R.id.edit_card_name_image_view);
+		editImageView.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View view) {
+				Log.i(TAG, "clicked on image view.");
+				valueTextView.setVisibility(View.GONE);
+				
+				EditText valueEditText = (EditText) findViewById(R.id.___name_et);
+				valueEditText.setVisibility(View.VISIBLE);
+				valueEditText.requestFocus();
+				
+				InputMethodManager imm = (InputMethodManager) 
+						getSystemService(Context.INPUT_METHOD_SERVICE);
+				imm.toggleSoftInput(InputMethodManager.SHOW_IMPLICIT, 0);
+			}
+		});
+
+		nameValueEditText = (EditText) findViewById(R.id.___name_et);
+		nameValueEditText.setOnEditorActionListener(new OnEditorActionListener() {
+
+			@Override
+			public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+				Log.i(TAG, "clicked done. "+actionCode+"<has to be is>"+actionId );
+				if (actionId == actionCode) {
+					Log.i(TAG, "code OK");
+					InputMethodManager imm = (InputMethodManager)getSystemService(
+						      Context.INPUT_METHOD_SERVICE);
+					imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+					nameValueEditText.setVisibility(View.GONE);
+					valueTextView.setVisibility(View.VISIBLE);
+					// TODO save
+				}
+				return false;
+			}
+		});
+		
+		mainScrollView = findViewById(R.id.main_scroll_view);
+		mainScrollView.setOnTouchListener(new OnTouchListener() {
+
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				Log.i(TAG, "clicked on layout");
+				if(v == mainScrollView){
+					Log.i(TAG, "hiding keyboard");
+					if(nameValueEditText.isShown()){
+						//TODO discard  changes
+						InputMethodManager imm = (InputMethodManager) 
+								getSystemService(Context.INPUT_METHOD_SERVICE);
+						imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+						nameValueEditText.setVisibility(View.GONE);
+						valueTextView.setVisibility(View.VISIBLE);
+					}
+				}
+				return true;
+			}
+		});
+	
 	}
 
 	@Override
@@ -53,8 +134,12 @@ public class ShowAllActivity extends Activity {
 	}
 	private void prepareTextViews(CardDescriptor cd) {
 		Resources resources = getResources();
-		setDescription(R.id.card_name_block,
-				resources.getString(R.string.card_name_string), cd.getCardName());
+		TextView columnDescriptionView = (TextView) 
+				findViewById(R.id.__name_description_tf);
+		columnDescriptionView.setText(resources.getString(R.string.card_name_string));
+		TextView columnValueView = (TextView) findViewById(R.id.__name_value_tf);
+		columnValueView.setText(cd.getCardName());
+		
 		setDescription(R.id.card_balance_block,
 				resources.getString(R.string.card_balance_string), cd.getBalanceString());
 		setDescription(R.id.card_number_block,
